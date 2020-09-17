20c6e5662a6dd6c8310e097e5bbb7a50cca75419
Mac <mac.kittipat@gmail.com>
2020-09-17 21:39:10 +0700

    Integrate Auth0 to Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {ProfileComponent} from './pages/profile/profile.component';
+import {AuthModule} from '@auth0/auth0-angular';
+import {LoginComponent} from './components/login/login.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent,
+    LoginComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: environment.auth0Domain,
+      clientId: environment.auth0ClientId
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
